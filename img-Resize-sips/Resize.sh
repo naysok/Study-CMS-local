53cd41f1cc68ef1858f9301934c739e2036c87b5
Naoki Yoshioka <ysok.ysok@gmail.com>
2017-11-24 04:53:19 +0900

    hoge

--- img-Resize-sips/Resize.sh
+++ img-Resize-sips/Resize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for file in *.jpg; do sips --resampleWidth 1600 $file --out ${file%.jpg}-thumb.jpg; done
+for file in *.jpg; do sips --resampleWidth 400 $file --out ${file%.jpg}-thumb.jpg; done
 
 
