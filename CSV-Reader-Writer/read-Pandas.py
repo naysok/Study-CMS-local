f8afcd2f24ca48a35b58574eca5f913ccf1f0555
Naoki Yoshioka <ysok.ysok@gmail.com>
2017-12-25 15:00:20 +0900

    writer

--- CSV-Reader-Writer/read-Pandas.py
+++ CSV-Reader-Writer/read-Pandas.py
@@ -1,19 +1,35 @@
 import pandas as pd
 
 data = pd.read_csv("171222-CSV.csv", header = None)
-# print(data) # all
+print("[ 全体 ]")
+print(data) # all
+
+print("-------------------------")
+
+
 
 #print(data[0][0])
 #print(data[1][1])
 
-a = 1 # 横
+a = 1
 
-print(data[a][0])
-print(data[a][1])
-print(data[a][2])
-print(data[a][3])
+# 縦
+#print("[ 縦 ]")
+#print(data[a][0])
+#print(data[a][1])
+#print(data[a][2])
+#print(data[a][3])
 
+#print("-------------------------")
+
+# 横
+print("[ 横 ]")
 print(data[0][a])
 print(data[1][a])
 print(data[2][a])
 print(data[3][a])
+print(data[4][a])
+print(data[5][a])
+
+
+print("-------------------------")
---
+++ CSV-Reader-Writer/write-from-csv.py
@@ -0,0 +1,6 @@
+str1 = "hoge hoge"
+
+f = open("index.html", "w") # write mode
+
+f.write(str1)
+f.close() # ファイルを閉じる
